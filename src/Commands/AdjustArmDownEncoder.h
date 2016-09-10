3a4cde8ceb95e73c1e6f67d84cdb2fd30a9ee40a
alexgt9123 <twigga04@gmail.com>
2016-09-10 18:13:51 -0400

    Completed port from java

--- src/Commands/AdjustArmDownEncoder.h
+++ src/Commands/AdjustArmDownEncoder.h
@@ -1,1 +1,19 @@
+#ifndef AdjustArmDownEncoder_H
+#define AdjustArmDownEncoder_H
 
+#include "../CommandBase.h"
+#include "WPILib.h"
+
+class AdjustArmDownEncoder: public CommandBase
+{
+public:
+	AdjustArmDownEncoder();
+	void Initialize();
+	void Execute();
+	bool IsFinished();
+	void End();
+	void Interrupted();
+	
+private:
+	
+}
