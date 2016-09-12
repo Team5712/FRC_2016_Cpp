bfd29455783cbafbdae52290649040961901639a
alexgt9123 <twigga04@gmail.com>
2016-09-12 14:29:45 -0400

    Finished Port

--- src/Commands/AdjustArmUpEncoder.cpp
+++ src/Commands/AdjustArmUpEncoder.cpp
@@ -1,1 +1,38 @@
+//Finished
 
+#include "AdjustArmUpEncoder.h"
+#include "WPILib.h"
+#include "../CommandBase.h"
+
+#include <iostream>
+
+void AdjustArmUpEncoder::AdjustArmUpEncoder() : CommandBase("AdjustArmUpEncoder")
+{
+	Requires(Robot::ShooterSystem.armUp());
+}
+
+void AdjustArmUpEncoder::Initialize()
+{
+	std::cout << "AdjustArmUpEncoder has initialized";
+}
+
+void AdjustArmUpEncoder::Execute()
+{
+	Robot::ShooterSystem::armUp();
+}
+
+bool AdjustArmUpEncoder::IsFinished()
+{
+	return Robot::ShooterSystem::isUp();
+}
+
+void AdjustArmUpEncoder::End()
+{
+	Robot::ShooterSystem::stopArm();
+}
+
+void AdjustArmUpEncoder::Interrupted()
+{
+	std::cout << "AdjustArmUpEncoder was interrupted!";
+	End();
+}
