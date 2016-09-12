f6f6dceab7fddb8c94f3226d6fa131e0f088acaf
alexgt9123 <twigga04@gmail.com>
2016-09-12 14:32:20 -0400

    Finished

--- src/Commands/AdjustArmUpEncoder.h
+++ src/Commands/AdjustArmUpEncoder.h
@@ -1,1 +1,21 @@
+#ifndef AdjustArmDownEncoder_H
+#define AdjustArmDownEncoder_H
 
+#include "../CommandBase.h"
+#include "WPILib.h"
+
+class AdjustArmUpEncoder: public CommandBase
+{
+public:
+	AdjustArmUpEncoder();
+	void Initialize();
+	void Execute();
+	bool IsFinished();
+	void End();
+	void Interrupted();
+	
+private:
+	
+}
+
+#endif
