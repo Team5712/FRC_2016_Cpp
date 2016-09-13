875a1ae5736c35ee8413ce22c99c4e8c095702f9
alexgt9123 <twigga04@gmail.com>
2016-09-12 20:58:05 -0400

    Finshed

--- src/Commands/StopShooter.cpp
+++ src/Commands/StopShooter.cpp
@@ -1,1 +1,39 @@
+//Finished
+
+#include "StopShooter.h"
+#include "WPILib.h"
+#include "../CommandBase.h"
+
+#include <iostream>
+
+void StopShooter::StopShooter() : CommandBase("StopShooter")
+{
+	Requires(Robot::ShooterSystem);
+}
+
+void StopShooter::Initialize()
+{
+	std::cout << "StopShooter has initialized";
+}
+
+void StopShooter::Execute()
+{
+	Robot::ShooterSystem::stopShooter();
+}
+
+bool StopShooter::IsFinished()
+{
+	return Robot::ShooterSystem::isStopped();
+}
+
+void StopShooter::End()
+{
+	
+}
+
+void StopShooter::Interrupted()
+{
+	std::cout << "StopShooter was interrupted!";
+	End();
+}
 
