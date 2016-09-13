accb6c04b0d8e3b0385e94e80bc241a8579f0129
alexgt9123 <twigga04@gmail.com>
2016-09-12 20:17:34 -0400

    Finished

--- src/Commands/InvertMotorsTrue.h
+++ src/Commands/InvertMotorsTrue.h
@@ -1,1 +1,22 @@
+#ifndef InvertMotorsTrue_H
+#define InvertMotorsTrue_H
+
+#include "../CommandBase.h"
+#include "WPILib.h"
+
+class InvertMotorsTrue: public CommandBase
+{
+public:
+	InvertMotorsTrue();
+	void Initialize();
+	void Execute();
+	bool IsFinished();
+	void End();
+	void Interrupted();
+	
+private:
+	
+}
+
+#endif
 
