36d6ac8c3f9973c40bfbcd37055d932aad5df4c8
alexgt9123 <twigga04@gmail.com>
2016-09-12 20:37:27 -0400

    Update AdjustArmUpEncoder.cpp

--- src/Commands/AdjustArmUpEncoder.cpp
+++ src/Commands/AdjustArmUpEncoder.cpp
@@ -8,7 +8,7 @@
 
 void AdjustArmUpEncoder::AdjustArmUpEncoder() : CommandBase("AdjustArmUpEncoder")
 {
-	Requires(Robot::ShooterSystem;
+	Requires(Robot::ShooterSystem);
 }
 
 void AdjustArmUpEncoder::Initialize()
