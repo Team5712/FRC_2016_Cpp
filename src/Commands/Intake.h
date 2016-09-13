268715a702e3229b170bb3d47c4485bcf38b54a0
alexgt9123 <twigga04@gmail.com>
2016-09-12 20:15:28 -0400

    Finished

--- src/Commands/Intake.h
+++ src/Commands/Intake.h
@@ -1,1 +1,21 @@
+#ifndef Intake_H
+#define Intake_H
 
+#include "../CommandBase.h"
+#include "WPILib.h"
+
+class Intake: public CommandBase
+{
+public:
+	Intake();
+	void Initialize();
+	void Execute();
+	bool IsFinished();
+	void End();
+	void Interrupted();
+	
+private:
+	
+}
+
+#endif
