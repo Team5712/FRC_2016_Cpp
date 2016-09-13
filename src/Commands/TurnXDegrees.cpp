7abc834e30bbae855536667b268af4036cb78784
alexgt9123 <twigga04@gmail.com>
2016-09-12 21:01:14 -0400

    Finished

--- src/Commands/TurnXDegrees.cpp
+++ src/Commands/TurnXDegrees.cpp
@@ -1,1 +1,39 @@
+//Finished
+
+#include "TurnXDegrees.h"
+#include "WPILib.h"
+#include "../CommandBase.h"
+
+#include <iostream>
+
+void TurnXDegrees::TurnXDegrees() : CommandBase("TurnXDegrees")
+{
+	Requires(Robot::DriveSystem);
+}
+
+void TurnXDegrees::Initialize()
+{
+	std::cout << "TurnXDegrees has initialized";
+}
+
+void TurnXDegrees::Execute()
+{
+	Robot::ShooterSystem::turnXDegrees();
+}
+
+bool TurnXDegrees::IsFinished()
+{
+	return Robot::ShooterSystem::isTurnedX();
+}
+
+void TurnXDegrees::End()
+{
+	Robot::ShooterSystem::stop();
+}
+
+void TurnXDegrees::Interrupted()
+{
+	std::cout << "TurnXDegrees was interrupted!";
+	End();
+}
 
