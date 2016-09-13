50a248ac8a23f8276c57e01c57c0449ef0929d9a
alexgt9123 <twigga04@gmail.com>
2016-09-12 20:22:35 -0400

    Finished

--- src/Commands/StopShooter.h
+++ src/Commands/StopShooter.h
@@ -1,1 +1,22 @@
+#ifndef StopShooter_H
+#define StopShooter_H
+
+#include "../CommandBase.h"
+#include "WPILib.h"
+
+class StopShooter: public CommandBase
+{
+public:
+	StopShooter();
+	void Initialize();
+	void Execute();
+	bool IsFinished();
+	void End();
+	void Interrupted();
+	
+private:
+	
+}
+
+#endif
 
