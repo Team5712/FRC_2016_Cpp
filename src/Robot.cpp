c82bd3e9426fd3ea8c5503ade40bc4a37e1149c5
alexgt9123 <twigga04@gmail.com>
2016-09-15 00:11:10 -0400

    Made skeleton finctions

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -1,1 +1,37 @@
+#include "RobotMap.h"
+#include "AHRS.h"
+#include "Encoder.h"
+#include "RobotDrive.h"
+#include "SerialPort.h"
+#include "VictorSP.h"
+#include "Subystem.h"
+#include "SmartDashboard.h"
 
+class Robot : public ItertatveRobot
+{
+private:
+	void RobotInit()
+	{
+		
+	}
+	
+	void AutonomousInit()
+	{
+		
+	}
+	
+	void AutonomousPeriodic()
+	{
+		
+	}
+	
+	void TeleopInit()
+	{
+		
+	}
+	
+	void TeleopPeriodic()
+	{
+		
+	}
+}
