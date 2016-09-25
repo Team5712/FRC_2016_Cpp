50315c245f29282a5ec72a064b9e2e24a2ef9e82
Rane Lafraze <lafrazerl@gmail.com>
2016-09-25 19:26:07 -0400

    Create base (template) header file for this subsystem

---
+++ src/Subsystems/DriveSystem.h
@@ -0,0 +1,19 @@
+#ifndef DRIVESYSTEM_H
+#define DRIVESYSTEM_H
+
+#include "Commands/Subsystem.h"
+#include "WPILib.h"
+
+
+class DriveSystem : public Subsystem
+{
+public:
+
+
+private:
+
+
+};
+
+
+#endif
