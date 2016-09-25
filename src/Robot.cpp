5dfabd2db3e851f3ddc9b7c0ecf8b48871ec6272
Rane Lafraze <lafrazerl@gmail.com>
2016-09-25 19:19:32 -0400

    Fix structure

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -1,3 +1,4 @@
+#include "Robot.h"
 #include "RobotMap.h"
 #include "AHRS.h"
 #include "Encoder.h"
@@ -7,96 +8,77 @@
 #include "Subystem.h"
 #include "SmartDashboard.h"
 
-class Robot : public ItertatveRobot
+
+
+void Robot::RobotInit()
 {
-public:
 
-static boolean IS_COMPETITION_ROBOT;
-	
-static DriveSubsystem driveSubsystem = new DriveSubsystem();	
-static ShooterSubsystem shooterSubsystem = new ShooterSubsystem();
-static PneumaticSubsystem pneumaticSubsystem = new PneumaticSubsystem();
-	
-static OI oi;
-	
-    //Autonomous commands
-    CommandGroup autonomousSelected;
-    double angleSelected;
-    
-    //Autonomous Selector
-    SendableChooser autoChooser, angleChooser;
-    
-    //Camera Variables
-    int sessionFront;
-    Image frame;
-
-	void RobotInit()
-	{
 	oi = new OI(); 
-		
-		autoChooser = new SendableChooser();
-		autoChooser.addDefault("Lowbar", new LowbarAutonomous());
-		autoChooser.addObject("Moat", new MoatAutonomous());
-		SmartDashboard.putData("Autonomous Mode Chooser", autoChooser);
-		
-		angleChooser = new SendableChooser();
-		angleChooser.addDefault("120", 120);
-		angleChooser.addObject("150", 150);
-		SmartDashboard.putData("Angle Chooser", angleChooser);
-		
-		pneumaticSubsystem.compressor.setClosedLoopControl(true);
-		
-		frame = NIVision.imaqCreateImage(NIVision.ImageType.IMAGE_RGB, 0);
-		sessionFront = NIVision.IMAQdxOpenCamera("cam0", NIVision.IMAQdxCameraControlMode.CameraControlModeController);
-		NIVision.IMAQdxConfigureGrab(sessionFront);
-		
-		driveSubsystem.resetGyro();
-		driveSubsystem.resetDriveEncoders();
-		shooterSubsystem.resetShooterEncoder();	
-	}
 	
-	void AutonomousInit()
-	{
+	autoChooser = new SendableChooser();
+	autoChooser.addDefault("Lowbar", new LowbarAutonomous());
+	autoChooser.addObject("Moat", new MoatAutonomous());
+	SmartDashboard.putData("Autonomous Mode Chooser", autoChooser);
+
+	angleChooser = new SendableChooser();
+	angleChooser.addDefault("120", 120);
+	angleChooser.addObject("150", 150);
+	SmartDashboard.putData("Angle Chooser", angleChooser);
+
+	pneumaticSubsystem.compressor.setClosedLoopControl(true);
+
+	frame = NIVision.imaqCreateImage(NIVision.ImageType.IMAGE_RGB, 0);
+	sessionFront = NIVision.IMAQdxOpenCamera("cam0", NIVision.IMAQdxCameraControlMode.CameraControlModeController);
+	NIVision.IMAQdxConfigureGrab(sessionFront);
+
+	driveSubsystem.resetGyro();
+	driveSubsystem.resetDriveEncoders();
+	shooterSubsystem.resetShooterEncoder();
+}
+
+void Robot::AutonomousInit()
+{
 	System.out.println("Autonomous Selected: " + autoChooser.getSelected());
-        System.out.println("Angle Selected: " + angleChooser.getSelected());
-        
-    	pneumaticSubsystem.in();
-        driveSubsystem.resetDriveEncoders();
-        driveSubsystem.resetGyro();
-        shooterSubsystem.resetShooterEncoder();
-        
-        autonomousSelected = (CommandGroup) autoChooser.getSelected();
-        angleSelected = (int) angleChooser.getSelected();
-        autonomousSelected.start();	
-	}
+	System.out.println("Angle Selected: " + angleChooser.getSelected());
+
+	pneumaticSubsystem.in();
+	driveSubsystem.resetDriveEncoders();
+	driveSubsystem.resetGyro();
+	shooterSubsystem.resetShooterEncoder();
 	
-	void AutonomousPeriodic()
-	{
+	autonomousSelected = (CommandGroup) autoChooser.getSelected();
+	angleSelected = (int) angleChooser.getSelected();
+	autonomousSelected.start();
+}
+
+void Robot::AutonomousPeriodic()
+{
 	Scheduler.getInstance().run();
-        
-        driveSubsystem.display();
-        shooterSubsystem.display();	
-	}
 	
-	void TeleopInit()
-	{
+	driveSubsystem.display();
+	shooterSubsystem.display();
+}
+
+void Robot::TeleopInit()
+{
 	if (autonomousSelected != null) autonomousSelected.cancel();
-    	
-    	driveSubsystem.resetDriveEncoders();
-    	driveSubsystem.resetGyro();	
-	}
-	
-	void TeleopPeriodic()
-	{
-	Scheduler.getInstance().run();
-    	
-    	driveSubsystem.drive.arcadeDrive(oi.driveStick);
-    	shooterSubsystem.shooter.set(oi.shootStick.getRawAxis(1));
-    	
-    	driveSubsystem.display();
-    	shooterSubsystem.display();
-		
-		NIVision.IMAQdxGrab(sessionFront, frame, 0);
-		CameraServer.getInstance().setImage(frame);	
+		driveSubsystem.resetDriveEncoders();
+		driveSubsystem.resetGyro();
 	}
 }
+
+void Robot::TeleopPeriodic()
+{
+	Scheduler.getInstance().run();
+	
+	driveSubsystem.drive.arcadeDrive(oi.driveStick);
+	shooterSubsystem.shooter.set(oi.shootStick.getRawAxis(1));
+
+	driveSubsystem.display();
+	shooterSubsystem.display();
+
+	NIVision.IMAQdxGrab(sessionFront, frame, 0);
+	CameraServer.getInstance().setImage(frame);
+}
+
+
