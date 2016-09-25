10d51edd81a19d0aa1c2ab143d87fe76fe4de336
Matt <loucks12345@gmail.com>
2016-09-25 17:16:53 -0400

    Update PneumaticSystem.h

--- src/Subsystems/PneumaticSystem.h
+++ src/Subsystems/PneumaticSystem.h
@@ -1,1 +1,17 @@
+#ifndef PNEUMATICSYSTEM_H
+#define PNEUMATICSYSTEM_H
 
+class PneumaticSystem : Subsystem {
+
+  public:
+  
+  PneumaticSubsystem();
+  initDefaultCommand();
+  shiftHigh();
+  shiftLow();
+  in();
+  out();
+  
+};
+
+#endif
